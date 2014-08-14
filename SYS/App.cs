fd2d4f7d6a069ee0a42aa3dfb2ae312dc93e7aa3
Visal <visal_chea@kredit.com.kh>
2014-08-14 16:57:15 +0700

    clean

--- SYS/App.cs
+++ SYS/App.cs
@@ -24,7 +24,7 @@ namespace kBit.ERP.SYS
         ////public static SM.frmUserList fUserList;
         ////public static SM.frmAuditLog fAuditLog;
 
-        //todo: public static FileLog AccessLog = new FileLog();
+        public static FileLog AccessLog = new FileLog();
 
         public static string ProcessID = System.Diagnostics.Process.GetCurrentProcess().Id.ToString();
 
@@ -46,12 +46,12 @@ namespace kBit.ERP.SYS
             }
             catch (Exception ex)
             {
-                //todo: uncomment ErrorLogFacade.LogToFile(ex);
+                ErrorLogFacade.LogToFile(ex);
             }
             // Database connection: open/test
             try
             {
-                fSplash.ShowMsg("Connecting to the SqlFacade...");
+                fSplash.ShowMsg("Connecting to the database ...");
                 //SqlFacade.Connection = new PetaPoco.Database(SqlFacade.ConnectionString, "NpgSql");                                                
                 SqlFacade.OpenConnection();
             }
@@ -67,16 +67,16 @@ namespace kBit.ERP.SYS
             SqlFacade.EnsureDBSetup();
 
             //Session            
-            ////session.Username = "Visal"; //todo: to be removed
-            ////session.MachineName = Environment.MachineName;
-            ////session.MachineUserName = Environment.UserName;
-            ////session.Version = version;
+            session.Username = "Visal"; //todo: to be removed
+            session.MachineName = Environment.MachineName;
+            session.MachineUserName = Environment.UserName;
+            session.Version = version;
             // Log
-            ////ErrorLogFacade.logFile.FileName = Path.Combine(Application.StartupPath, "Error.log");
-            ////AccessLog.FileName = Path.Combine(Application.StartupPath, "Access.log");            
-            ////AccessLog.Write(DateTime.Now.ToString("yyy-MM-dd ddd hh:mm:ss tt")  + " Application started. Process Id: " + ProcessID + ", Machine: " + session.MachineName + ", machine's username: " + session.MachineUserName + ", version: " + session.Version);
-            ////session.Id = SessionFacade.Save(session);
-            ////SessionLogFacade.Log(Type.Priority_Information, "Application", Type.Log_Launch, "Application started");
+            ErrorLogFacade.logFile.FileName = Path.Combine(Application.StartupPath, "Error.log");
+            AccessLog.FileName = Path.Combine(Application.StartupPath, "Access.log");
+            AccessLog.Write(DateTime.Now.ToString("yyy-MM-dd ddd hh:mm:ss tt") + " Application started. Process Id: " + ProcessID + ", Machine: " + session.MachineName + ", machine's username: " + session.MachineUserName + ", version: " + session.Version);
+            session.Id = SessionFacade.Save(session);
+            SessionLogFacade.Log(Type.Priority_Information, "Application", Type.Log_Launch, "Application started");
 
             return true;
         }
